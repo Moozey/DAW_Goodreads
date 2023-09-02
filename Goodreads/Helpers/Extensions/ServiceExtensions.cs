a85c8a877de0849cc49568018717d5df59a43570
Moozey <anavoinea821@yahoo.com>
2023-09-02 11:08:28 +0300

    added serviceExtensions in helpers

---
+++ Goodreads/Helpers/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Goodreads.Repositories.AuthorRepository;
+using Goodreads.Repositories.BookRepository;
+using Goodreads.Services.AuthorService;
+using Goodreads.Services.BookService;
+
+namespace Goodreads.Helpers.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<IBookRepository, BookRepository>();
+            services.AddTransient<IAuthorRepository, AuthorRepository>();
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddTransient<IBookService, BookService>();
+            services.AddTransient<IAuthorService, AuthorService>();
+
+            return services;
+        }
+    }
+}
--- Goodreads/Program.cs
+++ Goodreads/Program.cs
@@ -1,4 +1,6 @@
 using Goodreads.Data;
+using Goodreads.Repositories.BookRepository;
+using Goodreads.Services.BookService;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,10 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+//connect db
 builder.Services.AddDbContext<GoodreadsContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//repositories
+builder.Services.AddTransient<IBookRepository, BookRepository>();
+builder.Services.AddTransient<IBookService, BookService>();
+
 
 var app = builder.Build();
 
